0a33b0f5bbb1dd59e13dff977ae0c73698df9eaa
uday <udaykiran9147@gmail.com>
2025-11-21 11:01:31 +0530

    set app-icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,13 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata = {
   title: "Homli | house organizer",
-  description: "Manage groceries, chores, bills & calendars for multiple households in one app.",
+  description:
+    "Manage groceries, chores, bills & calendars for multiple households in one app.",
+  icons: {
+    icon: "app-icon.png",        
+    shortcut: "/icon.png",    
+    apple: "/icon.png",       
+  },
 };
 
 export default function RootLayout({ children }) {
